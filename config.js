c073ab9fc908baee855991d4e7dcadc7a9c53334
11200410092019 <86745942+11200410092019@users.noreply.github.com>
2021-07-01 04:53:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,6 +13,6 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Hôm nay có quà cho bé á.',
+    messDesc: 'Hôm nay có quà cho bé á <3 :3 ',
     btnAccept: 'Okiiiii lun nha <3',
 }
